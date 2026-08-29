41b0c11bf8ef927eeb4285b12e08fc69843a07e8
payneusmc07 <45081085+payneusmc07@users.noreply.github.com>
2026-08-28 22:51:10 -0700

    General Commit

--- validate-env.ts
+++ validate-env.ts
@@ -1,6 +1,7 @@
 import { validateEnv } from "@mpaynesecurity/rsbuild-plugin-hydra/env"
 import { object, coerce } from "zod"
 
+// Testing
 export const env = validateEnv(object({
 	PORT: coerce.number({
 		error: (issue) => {
